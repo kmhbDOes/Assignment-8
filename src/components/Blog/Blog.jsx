eef2bc1d07f9cf796be4289d01040a27d48f5d3f
kmhbDOes <kmhbipul@gmail.com>
2023-03-31 01:53:11 +0600

    More details displayed

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
 const Blog = (props) => {
-  const { thumbnail, authorName } = props.blog;
+  const { thumbnail, authorName, authorImage, publishDate } = props.blog;
   return (
     <div>
       <p className="my-4">
         <img src={thumbnail} alt="" />
+        <div>
+          <div>
+            <div className="flex ">
+              <img src={authorImage} alt="" />
+            </div>
+            <div>
+              <h2>{authorName}</h2>
+              <h2>{publishDate}</h2>
+            </div>
+          </div>
+          <div></div>
+        </div>
       </p>
     </div>
   );
