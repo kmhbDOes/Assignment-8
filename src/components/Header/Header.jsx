935bc58ec9ae9b7d3019d0c2cd93df4312d08bcc
kmhbDOes <kmhbipul@gmail.com>
2023-03-31 01:05:06 +0600

    Fake Data Added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,38 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center mx-8">
-      <h1 className="text-4xl font-bold">Upskill Camp</h1>
-      <div className="flex gap-x-4 items-center">
-        <a className="font-bold" href="">
-          Circular
-        </a>
-        <a className="font-bold" href="">
-          User
-        </a>
-        <img src="../../../public/client.png" alt="" />
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <h1 className="text-2xl md:text-4xl font-bold">Skillup Camp</h1>
+      </div>
+      <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+          ></div>
+        </div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="../../../public/client.png" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">Profile</a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
