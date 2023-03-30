991f8ea6c634f00ae63323e0b57fa6bee154b3df
kmhbDOes <kmhbipul@gmail.com>
2023-03-30 23:02:04 +0600

    Navlinks added in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,17 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex justify-between mx-8">
+    <div className="flex justify-between items-center mx-8">
       <h1 className="text-4xl font-bold">Upskill Camp</h1>
-      <img src="../../../public/client.png" alt="" />
+      <div className="flex gap-x-4 items-center">
+        <a className="font-bold" href="">
+          Circular
+        </a>
+        <a className="font-bold" href="">
+          User
+        </a>
+        <img src="../../../public/client.png" alt="" />
+      </div>
     </div>
   );
 };
