20e24e37269a2c995f1d35e6780226ad03afe5c2
kmhbDOes <kmhbipul@gmail.com>
2023-03-30 22:55:33 +0600

    Header Added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between mx-8">
+      <h1 className="text-4xl font-bold">Upskill Camp</h1>
+      <img src="../../../public/client.png" alt="" />
+    </div>
+  );
+};
+
+export default Header;
