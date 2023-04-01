0251d070b8398676ddb7df960265d78cb8f6192b
kmhbDOes <kmhbipul@gmail.com>
2023-04-01 14:32:19 +0600

    FUll responsive

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -3,15 +3,19 @@ import React from "react";
 const SideCart = ({ displayTime, bookmarkedBlogs }) => {
   return (
     <>
-      <div className="h-14 w-52 mx-auto border border-purple-400 text-purple-500 font-bold rounded-xl  bg-purple-100">
+      <div className="h-14 w-52 mx-auto border border-purple-400 text-purple-500 font-bold rounded-xl bg-purple-100">
         Spent Time On read: {displayTime} min
       </div>
-      <div className=" mx-auto border-4 rounded-xl shadow-lg bg-slate-200 font-bold  mt-3">
+      <div className=" mx-auto border-4 rounded-xl shadow-lg w-52 bg-slate-200 font-bold  mt-3">
         <div>
-          <h1>Bookmarked Blogs: {bookmarkedBlogs.length}</h1>
-          <ul>
+          <h1 className="px-8">Bookmarked Blogs: {bookmarkedBlogs.length}</h1>
+          <ul className="text-center">
             {bookmarkedBlogs &&
-              bookmarkedBlogs.map((title) => <li key={title}>{title}</li>)}
+              bookmarkedBlogs.map((title) => (
+                <li className="px-8" key={title}>
+                  {title}
+                </li>
+              ))}
           </ul>
         </div>
       </div>
